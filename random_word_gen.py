b3bf140ddb67a81cbd323753c623a90daaf991a6
manblue7 <45611354+manblue7@users.noreply.github.com>
2021-08-18 22:51:32 -0700

    Update random_word_gen.py

--- random_word_gen.py
+++ random_word_gen.py
@@ -1,32 +1,40 @@
 import random
+from tkinter.constants import CENTER
 import pandas as pd
 import json
 import dictionary
+import PySimpleGUI as sg
+import time
+
 
 
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-print('Press any button to generate random letters, then the letters will be searched for words in the dictionary, press "q" to quit\n')
+layout = [[sg.Spin(values=[x for x in range(1, 100)], initial_value=1, key='num')],
+[sg.Button('Generate', key='Gen')],
+[sg.Text(text='', size=(10, 1), justification=CENTER, key='textbox', enable_events=True)],
+[sg.Text(text='Words will appear here', key='words', size=(50, 20))]]
+window = sg.Window('Random Word Generator', layout, size=(400, 300))
 while True:
-    inp = input('Numnber of letters: ')
-    if inp.lower() == 'q':
+    event, values = window.read()
+    if event == sg.WIN_CLOSED:
         break
-    elif inp.isalpha() == False and  inp != '' and int(inp) < 1000001:
+    if event == 'Gen' and int(values['num'] < 100):
+        numWords = int(values['num'])
         letterString = ''
         words = []
-        for i in range(int(inp)):
+        for i in range(100000):
             ran = random.randint(0, 25)
             letterString += letters[ran]
         print(letterString)
-        print('Searching...')
-        df = pd.DataFrame(dictionary.dict, index=[0])
-        for word in df.columns:
+        print('Searching.....')
+        displayedWords = []
+        for word in dictionary.dict:
             if word in letterString and len(word) > 3:
-                print(word, end=', ')
                 words.append(word)
-        print('\nNumber of random words generated: ', len(words))
-        print('Here is a random word from the list: ', words[random.randint(0, len(words) - 1)])
-        continue
-    else:
-        continue
+                print(word, end=', ')
+        for word in range(numWords):
+            displayedWords.append(words[random.randint(0, len(words))])
+        print('\nNumber of random words generated: {}'.format(len(words)))
+        window['words'].update(displayedWords)
